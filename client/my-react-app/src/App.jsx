94d3208fa495386975a6cf9b99ae39b7ba830255
advait <developers@flipswap.in>
2026-06-06 13:21:55 +0530

    commit

--- client/my-react-app/src/App.jsx
+++ client/my-react-app/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import {HomePage}from './components/HomePage'
+
 import './App.css'
 
 function App() {
@@ -9,7 +8,7 @@ function App() {
 
   return (
     <>
-      <HomePage></HomePage>
+      <HomePage />
     </>
   )
 }
